42db70a1dcb34fb07aa9e20933deaa4271cd58c0
solarknight <zhouthanos@gmail.com>
2016-11-20 10:39:20 +0800

    add custom listener

--- src/main/java/com/thanos/springboot/Application.java
+++ src/main/java/com/thanos/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.thanos.springboot;
 
+import com.thanos.springboot.common.CustomApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(CustomApplicationListener.INSTANCE);
+        app.run(args);
     }
 }
