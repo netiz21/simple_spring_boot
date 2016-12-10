e90edbf278e2fb22e6eec232f9ac122e156eef17
peiheng.zph <peiheng.zph@alibaba-inc.com>
2016-12-10 15:49:25 +0800

    add monitor class

---
+++ monitor/src/main/java/com/thanos/btrace/script/RestControllerMethod.java
@@ -0,0 +1,32 @@
+package com.thanos.btrace.script;
+
+import com.sun.btrace.BTraceUtils.Strings;
+import com.sun.btrace.annotations.BTrace;
+import com.sun.btrace.annotations.Duration;
+import com.sun.btrace.annotations.Kind;
+import com.sun.btrace.annotations.Location;
+import com.sun.btrace.annotations.OnMethod;
+import com.sun.btrace.annotations.ProbeClassName;
+import com.sun.btrace.annotations.ProbeMethodName;
+
+import static com.sun.btrace.BTraceUtils.print;
+import static com.sun.btrace.BTraceUtils.println;
+
+/**
+ * @author peiheng.zph created on 16/12/10 下午12:16
+ * @version 1.0
+ */
+@BTrace
+public class RestControllerMethod {
+
+  @OnMethod(clazz = "@org.springframework.web.bind.annotation.RestController",
+      method = "/.*/",
+      location = @Location(Kind.RETURN))
+  public static void onMethod(@ProbeClassName String probClass, @ProbeMethodName String probMethod,
+                              @Duration long consume) {
+    print("entered " + probClass);
+    print("." + probMethod);
+    print(" consume " + Strings.str(consume / 1000) + " ms");
+    println();
+  }
+}
