f9a70a5ac4889ef23e3c6eabdb19f85e478db61f
solarknight <zhouthanos@gmail.com>
2017-03-27 15:40:13 +0800

    remove unused

--- log_support/src/main/java/com/thanos/monitor/ext/logback/base/condition/MonitorCondition.java
+++
@@ -1,8 +0,0 @@
-package com.thanos.monitor.ext.logback.base.condition;
-
-/**
- * @author peiheng.zph created on 17/2/12 下午4:16
- * @version 1.0
- */
-public interface MonitorCondition {
-}
--- log_support/src/main/java/com/thanos/monitor/ext/logback/base/targ/MonitorTarget.java
+++
@@ -1,8 +0,0 @@
-package com.thanos.monitor.ext.logback.base.targ;
-
-/**
- * @author peiheng.zph created on 17/2/12 下午4:15
- * @version 1.0
- */
-public interface MonitorTarget {
-}
