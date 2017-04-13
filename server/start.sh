ec19ae4af00d4c796a7fa86ebec76b83b0de717c
solarknight <zhouthanos@gmail.com>
2017-04-13 18:02:11 +0800

    update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export profile=${1}
+profile=${1}
 #export SPRING_PROFILES_ACTIVE=${1-dev}
 echo "========================"
 echo "using profile: " ${profile}
 echo "========================"
 mvn help:active-profiles
-mvn -Dprofile=${profile} -o spring-boot:run ${3}
+mvn spring-boot:run -Drun.jvmArguments="-Xms2048m -Xmx2048m -Dprofile=${profile}"
