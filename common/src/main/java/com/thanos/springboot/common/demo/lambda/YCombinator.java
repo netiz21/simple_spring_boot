7f9b2a56aa8410fdf1d673084c6400a59ae740f7
solarknight <zhouthanos@gmail.com>
2018-01-07 00:25:14 +0800

    add Y Combinator demo

---
+++ common/src/main/java/com/thanos/springboot/common/demo/lambda/YCombinator.java
@@ -0,0 +1,33 @@
+package com.thanos.springboot.common.demo.lambda;
+
+import java.util.function.Function;
+
+/**
+ * @author solarknight created on 2018/1/6 23:22
+ * @version 1.0
+ */
+public interface YCombinator {
+
+  interface RecursiveFunction<F> extends Function<RecursiveFunction<F>, F> {}
+
+  public static <A, B> Function<A, B> Y(Function<Function<A, B>, Function<A, B>> f) {
+    RecursiveFunction<Function<A, B>> r = w -> f.apply(x -> w.apply(w).apply(x));
+    return r.apply(r);
+  }
+
+  public static void main(String... arguments) {
+    Function<Integer, Integer> fib = Y(f -> n ->
+        (n <= 2)
+            ? 1
+            : (f.apply(n - 1) + f.apply(n - 2))
+    );
+    Function<Integer, Integer> fac = Y(f -> n ->
+        (n <= 1)
+            ? 1
+            : (n * f.apply(n - 1))
+    );
+
+    System.out.println("fib(10) = " + fib.apply(10));
+    System.out.println("fac(10) = " + fac.apply(10));
+  }
+}
